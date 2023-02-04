9f95e49a7b644960524aab762791da5664d83f3e
PARTHASTHANA08 <73220883+PARTHASTHANA08@users.noreply.github.com>
2023-02-04 20:01:42 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,39 @@
+import socket
+from threading import Thread 
+import time
+import os
+
+IP_ADDRESS = "127.0.0.1"
+PORT = 8050
+SERVER = None
+bufferSize=4096
+clients={}
+
+def acceptConnections():
+    global SERVER
+    global clients
+    while True:
+        client,addr = SERVER.accept()
+        client_name = client.recv(4096).decode().lower()
+        clients[client_name] ={
+            "clients" : client,
+            "address" : addr,
+            "connected_with" : "",
+            "file_name" : "",
+            "file_size" : 4096
+        }
+        print(f"Connection Established with {client_name} : {addr}")
+        thread = Thread(target=handleClient, args=(client,client_name))
+        thread.start()
+
+def setup():
+    print("\n\t\t\t\t\t\tIP MESSENGER")
+    global PORT,IP_ADDRESS,SERVER
+    SERVER = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    SERVER.bind((IP_ADDRESS,PORT))
+    SERVER.listen(100)
+    print("\t\t\t\t SERVER IS WAITING FOR INCOMING CONNECTIONS \n")
+    acceptConnections()
+
+setup_thread = Thread(target=setup)
+setup_thread.start()    
